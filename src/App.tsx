360fdd669373077964190b19feead78c7458ef25
Oyvind Moldestad <moldestad@gmail.com>
2018-11-11 17:09:54 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Fragment } from 'react';
+
+const App = () => (
+  <Fragment>
+    <h1>Hello World!</h1>
+    <h2>App: {APP_NAME}</h2>
+    <h2>Version: {APP_VERSION}</h2>
+  </Fragment>
+);
+
+export default App;
